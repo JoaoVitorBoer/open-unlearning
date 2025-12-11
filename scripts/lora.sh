19735a54fff5b04fbd028a6c7e6f23adcadb2677
Joao Vitor Boer Abitante <joaoavitor.abitante@gmail.com>
2025-12-11 17:12:57 -0300

    feat: enable training commands in lora.sh and configure trainer arguments

--- scripts/lora.sh
+++ scripts/lora.sh
@@ -41,19 +41,19 @@ for data_split in "${data_splits[@]}"; do
         echo ================== TRAINING =====================================
         echo ===================================================================
 
-        # CUDA_VISIBLE_DEVICES=0,1,2 accelerate launch --config_file configs/accelerate/default_config.yaml --num_processes=3 --main_process_port $MASTER_PORT \
-        # src/train.py --config-name=unlearn.yaml \
-        # experiment=unlearn/muse/default.yaml \
-        # adapter=lora \
-        # model=${model} \
-        # data_split=${data_split} \
-        # trainer=${trainer} \
-        # task_name=${task_name} \
-        # retain_logs_path=saves/eval/muse_Llama-2-7b-hf_${data_split}_retrain/MUSE_EVAL.json \
-        # trainer.args.per_device_train_batch_size=${per_device_train_batch_size} \
-        # trainer.args.gradient_accumulation_steps=${gradient_accumulation_steps} \
-        # trainer.args.ddp_find_unused_parameters=true \
-        # trainer.args.gradient_checkpointing=true
+        CUDA_VISIBLE_DEVICES=0,1,2 accelerate launch --config_file configs/accelerate/default_config.yaml --num_processes=3 --main_process_port $MASTER_PORT \
+        src/train.py --config-name=unlearn.yaml \
+        experiment=unlearn/muse/default.yaml \
+        adapter=lora \
+        model=${model} \
+        data_split=${data_split} \
+        trainer=${trainer} \
+        task_name=${task_name} \
+        retain_logs_path=saves/eval/muse_Llama-2-7b-hf_${data_split}_retrain/MUSE_EVAL.json \
+        trainer.args.per_device_train_batch_size=${per_device_train_batch_size} \
+        trainer.args.gradient_accumulation_steps=${gradient_accumulation_steps} \
+        trainer.args.ddp_find_unused_parameters=true \
+        trainer.args.gradient_checkpointing=true
 
         echo ===================================================================
         echo ================== EVALUATION =====================================
